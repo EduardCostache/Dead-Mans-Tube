d2955fccc722f5f0a6ae56e1f5d42a80d6b8c909
notAProgrammer <edwardcostache5@gmail.com>
2022-02-14 06:32:48 +0000

    added loading animation

---
+++ lib/Screens/loading.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:youtube_downloader/style.dart';
+
+class Loader extends StatelessWidget {
+  const Loader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: CustomTheme.backgroundColor(),
+      child: Center(
+        child: SpinKitFadingCircle(
+          color: CustomTheme.primaryColor(),
+          size: 50.0,
+        ),
+      ),
+    );
+  }
+}
