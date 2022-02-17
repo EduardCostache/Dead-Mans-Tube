9c4c3c70aa2dc105dc404dd0ef7749d90fb05f0f
notAProgrammer <edwardcostache5@gmail.com>
2022-02-17 00:45:18 +0000

    Change: loading screen animation

--- lib/Screens/loading_screen.dart
+++ lib/Screens/loading_screen.dart
@@ -10,7 +10,7 @@ class Loader extends StatelessWidget {
     return Container(
       color: CustomTheme.backgroundColor(),
       child: Center(
-        child: SpinKitFadingCircle(
+        child: SpinKitDualRing(
           color: CustomTheme.secondaryColor(),
           size: 50.0,
         ),
